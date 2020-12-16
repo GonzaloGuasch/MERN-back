19626e65dff7cdcecd14c79894745b1b3d511fa0
GonzaloGuasch <gonzaloguasch@outlook.com>
2020-12-16 19:44:16 -0300

    CORS & endpoint para votar con maximo de 10

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,20 +1,33 @@
+var cors = require('cors')
+
 module.exports = function(app, db) {
-    app.post('/peli', (req, res) => {
-        peli = {'nombre': req.body.name}
+    app.post('/peli', cors(), (req, res) => {
+        peli = {'title': req.body.title, 'poster_path': req.body.poster_path, 'votes': 0}
         db.collection('pelis').insert(peli);
         res.send({'status': '200'})
     });
     app.get('/peli/:name', async(req, res) => {
-        movie = await db.collection('pelis').find({'nombre': req.params.name}).toArray();
+        movie = await db.collection('pelis').find({'title': req.params.name}).toArray();
         res.send(movie)
     })
-     app.get('/peli/', async (req, res) => {
+     app.get('/pelis/', async (req, res) => {
         movies = await db.collection('pelis').find().toArray();
-        movies.map(aMovie => aMovie.name);
         res.send(movies)
     })
     app.delete('/peli/:name', async (req, res) => {
-        await db.collection('pelis').remove({'nombre': req.params.name});
+        await db.collection('pelis').remove({'title': req.params.name});
         res.send('Removed')
+    })
+    app.post('/vote', cors(), async (req, res) => {
+        if(req.body.votes < 10){
+            const movie_votes = await db.collection('pelis').findOne({'title': req.body.title});
+            await db.collection('pelis').updateOne(
+                { 'title': req.body.title },
+                {$set: {'votes':  movie_votes.votes + req.body.votes}}
+           );
+            res.send('updated!');
+        }else {
+            res.send('che no te pases de pillo');
+        }
     })
 };
--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const db = require('./config/db');
+const  cors = require('cors')
 
 const port = 8888;
 const app = express();
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 MongoClient.connect(db.url, (err, client) => {if(err) return console.log(err); require('./app/routes')(app, client.db('test'));});
