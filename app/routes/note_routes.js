46f09823590d460c1864d7787eb6b855a6067e95
GonzaloGuasch <gonzaloguasch@outlook.com>
2020-12-31 10:46:21 -0300

    Cambio como se vota, ahora se divide el voto por el total de todos los votos de la peli

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -2,7 +2,7 @@ var cors = require('cors')
 
 module.exports = function(app, db) {
     app.post('/peli', cors(), (req, res) => {
-        peli = {'title': req.body.title, 'poster_path': req.body.poster_path, 'votes': 0}
+        peli = {'title': req.body.title, 'poster_path': req.body.poster_path, 'votes': 0, 'total_votes': 0}
         db.collection('pelis').insert(peli);
         res.send({'status': '200'})
     });
@@ -19,11 +19,18 @@ module.exports = function(app, db) {
         res.send('Removed')
     })
     app.post('/vote', cors(), async (req, res) => {
-        if(req.body.votes < 10){
+        if(req.body.votes <= 10){
             const movie_votes = await db.collection('pelis').findOne({'title': req.body.title});
+            let votes;
+            if(movie_votes.total_votes === 0 ) {
+                votes = movie_votes.votes + req.body.votes;
+            } else {
+                let new_movie_votes = movie_votes.votes + req.body.votes;
+                votes = Math.floor(new_movie_votes / movie_votes.total_votes);
+            }
             await db.collection('pelis').updateOne(
                 { 'title': req.body.title },
-                {$set: {'votes':  movie_votes.votes + req.body.votes}}
+                {$set: {'votes':  votes, 'total_votes': movie_votes.total_votes + 1}}
            );
             res.send('updated!');
         }else {
